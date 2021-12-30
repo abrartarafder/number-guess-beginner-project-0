2b7023146e434f69aa52c4b34a7ab9abc6766aa2
abrartarafder <95165049+abrartarafder@users.noreply.github.com>
2021-12-30 13:31:11 -0500

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,1 +1,21 @@
+import random
 
+
+secret = random.randint(1,10)
+
+print("WELCOME TO RANDOM NUMBER GUESSING GAME")
+print("****************************************************")
+for i in range(10000000):
+
+    guess = int(input("Enter a number between 1 - 10 for guessing: "))
+
+    if guess == secret:
+        print("You have won the game congratulations!")
+        break
+    
+    if guess < secret:
+        print("Low number -- guess higher")
+
+
+    if guess > secret:
+        print("High number -- guess lower")
